5a044e1cb58ab30d7c64a17fb396b5d115e38e04
bagasrizki28 <bagasrizki700@gmail.com>
2023-03-03 22:02:57 +0700

    first commit

--- api/index.js
+++ api/index.js
@@ -1,21 +1,17 @@
-import express from "express";
 
+import express from "express";
 const app = express();
 
-// Middleware
 
-app.use((req, res, next) => {
+app.use((req,res,next)=>{
     console.log("ada request masuk");
-    // console.log(req.url);
     next();
-
 });
 
-// Route
-app.get("/api/path", (req, res) => {
-  res.send("assalamu'alaikum");
+app.get("/api/salam", (req,res)=>{
+        res.send("assalamualaikum");
 });
 
-app.listen(3000, () => {
-  console.log("Server sedang berjalan");
+app.listen(3000, ()=>{
+    console.log("server telah berjalan");
 });
